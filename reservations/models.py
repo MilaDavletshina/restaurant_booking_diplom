870c80b03cf1a2f0c3275f940ca654eb758a9a9f
ludmila davletshina <ludaplv@mail.ru>
2025-01-22 15:58:50 +0300

    добавлен status_choices, максимальное количество гостей

--- reservations/models.py
+++ reservations/models.py
@@ -1,16 +1,27 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
 from django.conf import settings
 
 
+NULLABLE = {"blank": True, "null": True}
+
+
 class Reservation(models.Model):
+    STATUS_PENDING = "В ожидании"
+    STATUS_CONFIRMED = "Подтвержден"
+
+    STATUS_CHOICES = [
+        (STATUS_PENDING, "статус: В ожидании"),
+        (STATUS_CONFIRMED, "статус: Подтвержден"),
+    ]
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateTimeField()
-    table_number = models.IntegerField()
-    status = models.CharField(max_length=20, default='pending')
+    guests = models.PositiveIntegerField(default='2', validators=[MaxValueValidator(25)], verbose_name="Количество персон", help_text='Введите количество гостей')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_PENDING)
 
 
 class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
+    name = models.CharField(max_length=100, verbose_name="Имя", help_text='Введите ваше имя')
+    email = models.EmailField(unique=True, verbose_name="Электронная почта", help_text='Введите вашу электронную почту')
+    message = models.TextField(**NULLABLE)
