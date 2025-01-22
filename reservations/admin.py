4e4e7d5776bf1e22acd757072e1684ed9a5aea87
ludmila davletshina <ludaplv@mail.ru>
2025-01-22 15:58:10 +0300

    изменен лист дисплей

--- reservations/admin.py
+++ reservations/admin.py
@@ -4,13 +4,13 @@ from .models import Reservation, Contact
 
 @admin.register(Reservation)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "table_number")
-    list_filter = ("user", "table_number",)
-    search_fields = ("user", "table_number",)
+    list_display = ("id", "user", "date", "guests", "status")
+    list_filter = ("user",)
+    search_fields = ("user",)
 
 
 @admin.register(Contact)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
+    list_display = ("id", "name", "email", "message")
     list_filter = ("name",)
     search_fields = ("user", "email",)
--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,4 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "email")
+    list_display = ("id", "email", "first_name", "last_name", "phone_number")
