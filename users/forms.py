7a214f569a41f71b4df602539015f91134f81709
ludmila davletshina <ludaplv@mail.ru>
2025-01-23 16:05:28 +0300

    добавлены формы приложения users

---
+++ users/forms.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
+from django.forms import ModelForm
+from django.urls import reverse_lazy
+
+from reservations.forms import StyleFormMixin
+from users.models import User
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    """Форма регистрации пользователя"""
+    class Meta:
+        model = User
+        fields = ("email", "password1", "password2")
+
+
+class UserForm(StyleFormMixin, UserChangeForm):
+    """Форма данных пользователя"""
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "phone_number",
+            "avatar",
+        )
+
+
+class UserUpdateForm(StyleFormMixin, ModelForm):
+    """Форма обновления данных пользователя"""
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "phone_number",
+            "avatar",
+        )
+        success_url = reverse_lazy("users:user_list")
+
+
+class UserForgotPasswordForm(PasswordResetForm):
+    """Форма запроса на восстановление пароля"""
+
+    def __init__(self, *args, **kwargs):
+        """Обновление стилей формы"""
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update(
+                {"class": "form-control", "autocomplete": "off"}
+            )
+
+
+class UserSetNewPasswordForm(SetPasswordForm):
+    """Форма изменения пароля пользователя после подтверждения"""
+
+    def __init__(self, *args, **kwargs):
+        """Обновление стилей формы"""
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update(
+                {"class": "form-control", "autocomplete": "off"}
+            )
