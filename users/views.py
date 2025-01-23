5dea794de990b404c483e7c5ade68820b4c22129
ludmila davletshina <ludaplv@mail.ru>
2025-01-23 16:05:00 +0300

    добавлены представления приложения users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,125 @@
-from django.shortcuts import render
+import secrets
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import PermissionDenied
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from config.settings import EMAIL_HOST_USER
+from users.forms import UserRegisterForm, UserForgotPasswordForm, UserSetNewPasswordForm, UserForm, UserUpdateForm
+from users.models import User
+
+
+class UserCreateView(CreateView):
+    """Переход после регистрации на страницу пользователя"""
+
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("users:login")
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f"http://{host}/users/email-confirm/{token}/"
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Привет! Для подтверждения почты перейдите по ссылке {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
+
+
+def email_verification(request, token):
+    """Подтверждение email"""
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse("users:login"))
+
+
+class UserForgotPasswordView(SuccessMessageMixin, PasswordResetView):
+    """Представление по сбросу пароля по почте"""
+
+    form_class = UserForgotPasswordForm
+    template_name = "users/user_password_reset.html"
+    success_url = reverse_lazy("users:login")
+    success_message = (
+        "Письмо с инструкцией по восстановлению пароля отправлено на ваш email"
+    )
+    subject_template_name = "users/email/password_subject_reset_mail.txt"
+    email_template_name = "users/email/password_reset_mail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Запрос на восстановление пароля"
+        return context
+
+
+class UserPasswordResetConfirmView(SuccessMessageMixin, PasswordResetConfirmView):
+    """Представление установки нового пароля"""
+
+    form_class = UserSetNewPasswordForm
+    template_name = "users/user_password_set_new.html"
+    success_url = reverse_lazy("users:login")
+    success_message = "Пароль успешно изменен. Можете авторизоваться на сайте."
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Установить новый пароль"
+        return context
+
+
+class UserListView(ListView, LoginRequiredMixin):
+    """Страница пользователей"""
+
+    model = User
+    template_name = "users/user_list.html"
+
+
+class UserDetailView(DetailView, LoginRequiredMixin):
+    """Просмотр пользователя"""
+
+    model = User
+    form_class = UserUpdateForm
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.request.user.is_superuser:
+            return self.object
+        raise PermissionDenied
+
+
+class UserUpdateView(UpdateView, LoginRequiredMixin):
+    """Пользователь - обновление"""
+
+    model = User
+    form_class = UserUpdateForm
+    success_url = reverse_lazy("users:user_list")
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.request.user.is_superuser:
+            return self.object
+        raise PermissionDenied
+
+
+class UserDeleteView(DeleteView):
+    """Пользователь - удаление"""
+
+    model = User
+    success_url = reverse_lazy("users:user_list")
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.request.user.is_superuser:
+            return self.object
+        raise PermissionDenied
