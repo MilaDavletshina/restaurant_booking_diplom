38f4ede4e28b1c5a7c3ff558a82320f7273d020e
ludmila davletshina <ludaplv@mail.ru>
2025-01-23 16:07:19 +0300

    добавлены пути

--- config/urls.py
+++ config/urls.py
@@ -9,3 +9,5 @@ urlpatterns = [
     path('reservations/', include('reservations.urls', namespace='reservations'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- reservations/urls.py
+++ reservations/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from . import views
-
+from .views import MainView, Contacts, Message, AboutListView, ReservationListView, ReservationDetailView, \
+    ReservationCreateView, ReservationUpdateView, ReservationDeleteView
 
 app_name = 'reservations'
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    # path('home/', views.home, name='home'),
+    path("", MainView.as_view(), name="main"),
+    path("contacts/", Contacts.as_view(), name="contacts"),
+    path("message/", Message.as_view(), name="message"),
+    path("about/", AboutListView.as_view(), name="about"),
+
+    path("reservation/", ReservationListView(), name="reservation_list"),
+    path("reservation/<int:pk>/", ReservationDetailView.as_view(), name="reservation_detail"),
+    path("reservation/create/", ReservationCreateView.as_view(), name="reservation_create"),
+    path("reservation/<int:pk>/update/", ReservationUpdateView.as_view(), name="reservation_update"),
+    path("reservation/<int:pk>/delete/", ReservationDeleteView.as_view(), name="reservation_delete"),
 ]
