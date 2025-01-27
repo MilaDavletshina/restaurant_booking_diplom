805716a65e6c34fda7667aa13a1b0f00e6e88c40
ludmila davletshina <ludaplv@mail.ru>
2025-01-27 15:50:21 +0300

    добавлен admin.register к модели Restaurant

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
-from .models import Reservation, Contact
+from .models import Reservation, Restaurant, Contact
 
 
 @admin.register(Reservation)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "date", "guests", "status")
-    list_filter = ("user",)
-    search_fields = ("user",)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "restaurant", "date", "guests", "status")
+    list_filter = ("user", "restaurant", )
+    search_fields = ("user", "restaurant", )
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description")
+    list_filter = ("name",)
+    search_fields = ("name",)
 
 
 @admin.register(Contact)
-class UserAdmin(admin.ModelAdmin):
+class ContactAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "email", "message")
     list_filter = ("name",)
     search_fields = ("user", "email",)
