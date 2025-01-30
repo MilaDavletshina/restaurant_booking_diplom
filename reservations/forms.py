4b9f3eb1e752037d2223b4ec5715040483174062
ludmila davletshina <ludaplv@mail.ru>
2025-01-30 11:31:19 +0300

    изменена форма бронирования столика.

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm
-
+from django.forms import ModelForm, DateTimeField, DateInput
 from reservations.models import Reservation, Restaurant
+from django import forms
 
 
 class StyleFormMixin:
@@ -10,13 +10,33 @@ class StyleFormMixin:
             field.widget.attrs["class"] = "form-control"
 
 
-class ReservationForm(StyleFormMixin, ModelForm):
-    class Meta:
-        model = Reservation
-        fields = "__all__"
-
-
 class RestaurantForm(StyleFormMixin, ModelForm):
+    """Форма создания нового ресторана."""
     class Meta:
         model = Restaurant
         fields = "__all__"
+
+
+class ReservationForm(ModelForm):
+    """Форма бронирования столика."""
+
+    class Meta:
+        """Стилизация формы бронирования столика."""
+        model = Reservation
+        fields = ["user", "restaurant", "table", 'reserved_at', 'customer_name', "customer_contact"]
+        widgets = {
+            'reserved_at': forms.DateTimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Выберите дату и время',
+                'type': 'datetime-local'  # для выбора даты и времени
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReservationForm, self).__init__(*args, **kwargs)
+        self.fields["user"].widget.attrs.update({'class': 'form-control'})
+        self.fields["restaurant"].widget.attrs.update({'class': 'form-control'})
+        self.fields["table"].widget.attrs.update({'class': 'form-control'})
+        self.fields["customer_name"].widget.attrs.update({'class': 'form-control', 'placeholder': 'Уточните Ваше имя'})
+        self.fields["customer_contact"].widget.attrs.update({'class': 'form-control', 'placeholder': 'Укажите контактную информацию'})
+
