103a1bd6ee1cae0896bced58a7da51b38408b1f3
ludmila davletshina <ludaplv@mail.ru>
2025-01-30 11:42:17 +0300

    RestaurantAdmin убрано поле user, restaurant

--- reservations/admin.py
+++ reservations/admin.py
@@ -4,9 +4,9 @@ from .models import Reservation, Restaurant, Contact, Table
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "restaurant", "reserved_at", "customer_name", "customer_contact")
-    list_filter = ("customer_name", "restaurant", )
-    search_fields = ("customer_name", "restaurant", )
+    list_display = ("id", "reserved_at", "customer_name", "customer_contact")
+    list_filter = ("customer_name", "reserved_at", )
+    search_fields = ("customer_name", "reserved_at", )
 
 
 @admin.register(Restaurant)
