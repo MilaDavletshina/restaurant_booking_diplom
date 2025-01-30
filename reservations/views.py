1a643803c13c064fe7535adc79aa5b8446726caa
ludmila davletshina <ludaplv@mail.ru>
2025-01-30 14:57:44 +0300

    добавлено сохранение при успешном бронировании

--- reservations/views.py
+++ reservations/views.py
@@ -1,21 +1,22 @@
-from django.shortcuts import render
+from datetime import timezone
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.views.generic import (TemplateView, ListView, CreateView)
 from reservations.forms import ReservationForm
 from reservations.models import Reservation, Restaurant
 
+# DetailView, , UpdateView, DeleteView
+
 
 def home(request):
-    """Основной шаблон"""
+    """Основной шаблон."""
     return render(request, 'home.html')
 
 
 class Contacts(TemplateView):
-    """Шаблон контакты"""
-
+    """Шаблон контакты."""
     template_name = "reservations/contacts.html"
 
     def contacts(request):
@@ -27,72 +28,83 @@ class Contacts(TemplateView):
 
 
 class Feedback(TemplateView):
-    """Шаблон обратная связь"""
-
+    """Шаблон обратной связи."""
     template_name = "reservations/feedback.html"
 
 
 class Message(TemplateView):
-    """Страница ответа на отправленное сообщение"""
-
+    """Шаблон ответа на обратную связь."""
     template_name = "reservations/message.html"
 
 
 class MainView(ListView):
-    """Главная страница"""
+    """Главная страница."""
     model = Restaurant
     template_name = "reservations/main.html"
 
 
 class AboutView(ListView):
-    """Страница о ресторане"""
-
+    """Страница о ресторане."""
     model = Restaurant
     template_name = "reservations/about.html"
 
 
 class ReservationListView(ListView):
-    """Страница бронирования"""
-
+    """Страница бронирования."""
     model = Reservation
-
-    def get_object(self, queryset=None):
-        self.object = super().get_object(queryset)
-        self.object.save()
-        return self.object
+    template_name = "reservations/reservation_list.html"
+    context_object_name = 'reservations'
 
 
-class ReservationDetailView(DetailView, LoginRequiredMixin):
-    """Бронирование, просмотр"""
-
+class ReservationCreateView(CreateView):
+    """Страница создания бронирования."""
     model = Reservation
     form_class = ReservationForm
-
-
-class ReservationCreateView(CreateView, LoginRequiredMixin):
-    """Бронирование, создание"""
-
-    model = Reservation
-    form_class = ReservationForm
-    success_url = reverse_lazy("reservations:main")
+    success_url = reverse_lazy("reservations:reservation_list")
 
     def form_valid(self, form):
-        reservation = form.save()
-        reservation.save()
+        # Сохраняем объект Reservation
+        self.object = form.save()
+
+        # Добавляем сообщение об успешном бронировании
+        messages.success(self.request,
+                         f"Бронирование столика №{self.object.table.number} на {self.object.reserved_at} успешно создано.")
 
         return super().form_valid(form)
 
-
-class ReservationUpdateView(UpdateView, LoginRequiredMixin):
-    """Бронирование, обновление"""
-
-    model = Reservation
-    form_class = ReservationForm
-    success_url = reverse_lazy("reservations:main")
+    def form_invalid(self, form):
+        # Если форма не валидна, показываем ее с ошибками
+        return super().form_invalid(form)
 
 
-class ReservationDeleteView(DeleteView):
-    """Получатель рассылки - удаление"""
 
-    model = Reservation
-    success_url = reverse_lazy("reservations:main")
+
+
+
+# class ReservationDetailView(DetailView, LoginRequiredMixin):
+#     """Бронирование, просмотр"""
+#
+#     model = Reservation
+#     form_class = ReservationForm
+#
+#
+
+
+
+#
+#
+# class ReservationUpdateView(UpdateView, LoginRequiredMixin):
+#     """Бронирование, обновление"""
+#
+#     model = Reservation
+#     form_class = ReservationForm
+#     success_url = reverse_lazy("reservations:main")
+#
+#
+# class ReservationDeleteView(DeleteView):
+#     """Получатель рассылки - удаление"""
+#
+#     model = Reservation
+#     success_url = reverse_lazy("reservations:main")
+
+
