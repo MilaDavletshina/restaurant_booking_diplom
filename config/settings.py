e997a0d0cf0885183214c238fdab34830553af31
ludmila davletshina <ludaplv@mail.ru>
2025-02-07 16:34:57 +0300

    изменены настройки ДБ

--- config/settings.py
+++ config/settings.py
@@ -62,11 +62,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "POSTGRES_DB": os.getenv("POSTGRES_DB"),
-        "POSTGRES_USER": os.getenv("POSTGRES_USER"),
-        "POSTGRESS_PASSWORD": os.getenv("POSTGRESS_PASSWORD"),
-        "POSTGRESS_HOST": os.getenv("POSTGRESS_HOST"),
-        "POSTGRESS_PORT": os.getenv("POSTGRESS_PORT"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRESS_PASSWORD"),
+        "HOST": os.getenv("POSTGRESS_HOST"),
+        "PORT": os.getenv("POSTGRESS_PORT"),
     }
 }
 
